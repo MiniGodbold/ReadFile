0d796ddb4f9ce158a770a11fbf95ec5157f9a750
MiniGodbold <79763978+MiniGodbold@users.noreply.github.com>
2021-03-07 16:51:17 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Voting_System
+{
+    class open_file
+    {
+        static void Main(string[] args)
+        {
+            string readText = File.ReadAllText(@"c:\Users/owengodbold/Projects/Voting_System/Data");
+            var result = readText.Replace("; ", Environment.NewLine);
+            Console.WriteLine(result);
+        }
+    }
+}
